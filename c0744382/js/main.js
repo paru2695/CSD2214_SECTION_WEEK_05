5adfa5cdb4339ae788de455298d2a02949138af1
CSD2214_SECTION5_WEEK_05 <visualstudio@230-PC12>
2019-02-07 11:44:34 -0500

    11:44am

--- c0744382/js/main.js
+++ c0744382/js/main.js
@@ -1,1 +1,12 @@
 ﻿// Your code here!
+function myfunction(a, b) {
+    var a = 5;
+    var b = 7;
+    return a + b; //the function returns the product.
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "green"];
+    console.log(pens);
+}
